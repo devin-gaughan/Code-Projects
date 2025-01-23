ae3b72abc751ef195b60fe5e48cebe91ddd118ed
Devin Gaughan <12529122+devin-gaughan@users.noreply.github.com>
2025-01-23 22:34:12 +0700

    Added 3D plotting to visualization.py
    Added 3D plotting to visualization.py

--- OSU/WINT2025/CS406/auraeon-atomic-crystal-sim-in-python/visualization.py
+++ OSU/WINT2025/CS406/auraeon-atomic-crystal-sim-in-python/visualization.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
 
 def plot_2d_lattice(x, y, title="2D Lattice"):
     """ plots a 2D lattice using a scatter plot in matplotlib """
@@ -11,4 +12,13 @@ def plot_2d_lattice(x, y, title="2D Lattice"):
     plt.grid(True)
     plt.show()
     
-        
+def plot_3d_lattice(x, y, z, title="3D Lattice"):
+    import matplotlib.pyplot as plt
+    fig = plt.figure(figsize=(7, 5))
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(x, y, z, c='red', s=15)
+    ax.set_title(title)
+    ax.set_xlabel("X (Å)")
+    ax.set_ylabel("Y (Å)")
+    ax.set_zlabel("Z (Å)")
+    plt.show()
